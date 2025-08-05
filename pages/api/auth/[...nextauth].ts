e4a88dcf1525b1ba74bd28ecdbf2709a56d82109
Shalina Arpheliasari <shalinarph@gmail.com>
2025-08-05 16:58:53 +0700

    fetcher, next auth, and accessing database by Supabase

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,44 @@
+import NextAuth from "next-auth";
+import bcrypt from "bcryptjs";
+import { SupabaseAdapter } from "@next-auth/supabase-adapter";
+import CredentialsProvider from "next-auth/providers/credentials";
+import supabase from "../../../libs/supabase";
+
+export const handler = {
+    adapter: SupabaseAdapter({
+        url: process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        secret: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    }),
+
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "email" },
+                password: { label: "Password", type: "password" },
+            },
+            async authorize(credentials) {
+                if (!credentials?.email || !credentials?.password) {
+                    throw new Error('Email and password are required')
+                }
+
+                const user = await supabase.from('users').select('*').eq('email', credentials.email).single();
+                if (!user || !user.data.hashedpassword) {
+                    throw new Error('No user found with this email') 
+                }
+
+                const isValidPassword = await bcrypt.compare(credentials.password, user.data.hashedpassword)
+                if (!isValidPassword) {
+                    throw new Error('Invalid password')
+                }
+
+                return {
+                    id: user.data.id,
+                    email: user.data.email
+                }
+            },
+        })
+    ],
+}
+
+export default NextAuth(handler);
