d755aa10abc0496cf2e49fd800467668a6f7fc00
Shalina Arpheliasari <shalinarph@gmail.com>
2025-08-06 23:02:45 +0700

    debugging register.ts

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -14,21 +14,36 @@ export default async function handler(
     try {
         const { email, username, name, password } = req.body
         const hashedpassword = await bcrypt.hash(password, 8)
-        const user = await supabase.auth.signUp({
-            email: email,
-            password: hashedpassword,
-            options: {
-                data: {
-                    username: username,
-                    name: name,
-                }
+
+        // Check if this email is already registered
+        const { data: existingUser } = await supabase
+            .from('users')
+            .select('*')
+            .eq('email', email)
+            .maybeSingle();
+
+            if (existingUser) {
+                return res.status(400).json({ error: 'Email already registered' })
             }
-        })
-        
-        return res.status(200).json(user.data.user)
+
+        // Insert new user into the database
+        const user = await supabase
+            .from('users')
+            .insert([
+                {
+                    email,
+                    username,
+                    name,
+                    password: hashedpassword,
+                }
+            ])
+            .select()
+            .single();
+
+        return res.status(201).json(user);
     } catch (error) {
         console.log(error);
-        return res.status(400).json({ error: 'Failed to register user' })
+        return res.status(500).json({ error: 'Internal server error' });
     }
     
 }
