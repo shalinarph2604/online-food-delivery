727816a24eaa603e69b34031bbf045e148045b06
Shalina Arpheliasari <shalinarph@gmail.com>
2025-10-09 00:16:52 +0700

    on progress; developing button and modal

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,1 +1,39 @@
 // ambil dari ui/button.tsx
+interface ButtonProps {
+    label: string;
+    primary: string;
+    secondary?: boolean;
+    disabled?: boolean;
+    large?: boolean;
+    onClick: () => void
+}
+
+const Button: React.FC<ButtonProps> = ({
+    label,
+    primary,
+    secondary,
+    disabled,
+    small,
+    onClick
+}) => {
+    return (
+        <button 
+            disabled={disabled}
+            onClick={onClick}
+            className={`
+                disabled:opacity-70
+                disabled:cursor-not-allowed
+                hover: opacity-80
+                transition
+                border-2
+                rounded-full
+                ${primary ? 'bg-purple-500 text-white border-purple-500' : ''}
+                ${secondary ? 'bg-white text-purple-500 border-purple-500' : ''}
+            `}
+        >
+            {label}
+        </button>
+    )
+}
+
+export default Button
--- components/modals/LoginModal.tsx
+++ components/modals/LoginModal.tsx
@@ -1,1 +1,5 @@
 // ini simple aja, dialog sama input aja, sama button paling
+import useLoginModal from "@/hooks/useLoginModal";
+import useRegisterModal from "@/hooks/useRegister";
+
+import { useState, useCallback } from "react";
