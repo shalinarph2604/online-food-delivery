04f18c1f2cf1a856511a972d08cbb7b770ad1f04
Shalina Arpheliasari <shalinarph@gmail.com>
2025-10-15 16:22:57 +0700

    change the useTopUp.ts to useTopUpModal.ts

--- components/modals/LoginModal.tsx
+++ components/modals/LoginModal.tsx
@@ -1,5 +1,7 @@
 // ini simple aja, dialog sama input aja, sama button paling
 import useLoginModal from "@/hooks/useLoginModal";
 import useRegisterModal from "@/hooks/useRegister";
+import Modal from "../Modal";
 
 import { useState, useCallback } from "react";
+
