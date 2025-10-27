2eaf621ec72b6ec56ca286653e2fa776a925adf0
Shalina Arpheliasari <shalinarph@gmail.com>
2025-10-27 11:33:41 +0700

    debuggin NavBart and _app.tsx

--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -2,10 +2,10 @@ import { useRouter } from 'next/router'
 import { useCallback } from 'react'
 import useCurrentUser from '@/hooks/useCurrentUser'
 import useRegisterModal from '@/hooks/useRegisterModal'
+import useLoginModal from '@/hooks/useLoginModal'
 
 import { FaHome, FaShoppingCart, FaBox, FaUser, FaSignOutAlt, FaSignInAlt } from 'react-icons/fa'
 import SearchBar from './SearchBar'
-import RegisterModal from '../modals/RegisterModal'
 
 interface NavbarProps {
     onSearch?: (query: string) => void;
@@ -14,6 +14,7 @@ interface NavbarProps {
 const Navbar: React.FC<NavbarProps> = ({ onSearch }) => {
     const router = useRouter()
     const registerModal = useRegisterModal()
+    const loginModal = useLoginModal()
 
     const showSearchBar = router.pathname === '/'
     const { user } = useCurrentUser()
@@ -24,65 +25,70 @@ const Navbar: React.FC<NavbarProps> = ({ onSearch }) => {
 
     const handleLogout = useCallback(async() => {
         await localStorage.removeItem('token')
-        
-        registerModal.onOpen()
-    }, [registerModal])
+        router.push('/')
+        window.location.reload()
+    }, [router])
 
     return (
-        <nav className="
-            fixed
-            left-1/2
-            transform
-            -translate-x-1/2
-            bg-purple-900
-            text-white
-            backdrop-blur-md
-            shadow-md
-            border border-purple-900
-            w-full
-            mx-auto
-            z-50
-            gap-10
-            flex
-            items-center
-            justify-between
-            px-6 py-3">
-            {/*The left side */}
-            <div className="flex items-center gap-4">
-                <button onClick={() => handleNavigate('/')} aria-label='Home'>
-                    <FaHome className="text-white text-xl hover:scale-110 transition"/>
-                </button>
-                <button onClick={() => handleNavigate('/cart')} aria-label='Cart'>
-                    <FaShoppingCart className="text-white text-xl hover:scale-110 transition"/>
-                </button>
-                <button onClick={() => handleNavigate('/orders')} aria-label='Orders'>
-                    <FaBox className="text-white text-xl hover:scale-110 transition"/>
-                </button>
-            </div>
-
-            {/*The middle side (search bar) */}
-            <div className="flex-1 mx-4 hidden md:block">
-                {showSearchBar && <SearchBar onSearch={onSearch} />}
-            </div>
-
-            {/*The right side */}
-            {user ? (
-                <div>
-                    <button onClick={RegisterModal} aria-label="Login">
-                        <FaSignInAlt className="text-white text-xl hover:scale-110 transition" />
-                    </button>
-                </div>
-            ) : (
+        <>
+            <nav className="
+                fixed
+                left-1/2
+                transform
+                -translate-x-1/2
+                bg-purple-900
+                text-white
+                backdrop-blur-md
+                shadow-md
+                border border-purple-900
+                w-full
+                mx-auto
+                z-50
+                gap-10
+                flex
+                items-center
+                justify-between
+                px-6 py-3">
+                {/*The left side */}
                 <div className="flex items-center gap-4">
-                    <button onClick={() => handleNavigate('/profile')} aria-label='Profile'>
-                        <FaUser className="text-white text-xl hover:scale-110 transition"/>
+                    <button onClick={() => handleNavigate('/')} aria-label='Home'>
+                        <FaHome className="text-white text-xl hover:scale-110 transition"/>
                     </button>
-                    <button onClick={() => handleLogout()} aria-label='Logout'>
-                        <FaSignOutAlt className="text-white text-xl hover:scale-110 transition"/>
-                    </button> 
+                    <button onClick={() => handleNavigate('/cart-general')} aria-label='Cart'>
+                        <FaShoppingCart className="text-white text-xl hover:scale-110 transition"/>
+                    </button>
+                    <button onClick={() => handleNavigate('/orders')} aria-label='Orders'>
+                        <FaBox className="text-white text-xl hover:scale-110 transition"/>
+                    </button>
                 </div>
-            )}  
-        </nav>
+
+                {/*The middle side (search bar) */}
+                <div className="flex-1 mx-4 hidden md:block">
+                    {showSearchBar && <SearchBar onSearch={onSearch} />}
+                </div>
+
+                {/*The right side */}
+                {user ? (
+                    <div className="flex items-center gap-4">
+                        <button onClick={() => handleNavigate('/profile')} aria-label='Profile'>
+                            <FaUser className="text-white text-xl hover:scale-110 transition"/>
+                        </button>
+                        <button onClick={() => handleLogout()} aria-label='Logout'>
+                            <FaSignOutAlt className="text-white text-xl hover:scale-110 transition"/>
+                        </button> 
+                    </div>
+                ) : (
+                    <div className="flex items-center gap-4">
+                        <button onClick={loginModal.onOpen} aria-label="Login">
+                            <FaSignInAlt className="text-white text-xl hover:scale-110 transition" />
+                        </button>
+                        <button onClick={registerModal.onOpen} aria-label="Register">
+                            <FaUser className="text-white text-xl hover:scale-110 transition" />
+                        </button>
+                    </div>
+                )}  
+            </nav>
+        </>
     )
 }
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,21 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app'ss
 import { SessionProvider } from 'next-auth/react'
+import LoginModal from '@/components/modals/LoginModal'
+import RegisterModal from '@/components/modals/RegisterModal'
+import CartModal from '@/components/modals/CartModal'
+import TopUpModal from '@/components/modals/TopUpModal'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
+      <LoginModal />
+      <RegisterModal />
+      <CartModal />
+      <TopUpModal />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </SessionProvider>
   )
 }
