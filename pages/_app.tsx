5000040c5a727cd43a914a204b2ba27717f1b8bb
Shalina Arpheliasari <shalinarph@gmail.com>
2025-10-27 11:34:04 +0700

    finished _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import LoginModal from '@/components/modals/LoginModal'
 import RegisterModal from '@/components/modals/RegisterModal'
 import CartModal from '@/components/modals/CartModal'
 import TopUpModal from '@/components/modals/TopUpModal'
+import Layout from '@/components/Layout'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
